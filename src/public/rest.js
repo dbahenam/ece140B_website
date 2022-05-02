cc5e29fa754c3742a015d9c4bd6f7297c1d170f2
David Bahena Moctezuma <62975222+dbahenam@users.noreply.github.com>
2022-05-02 14:31:06 -0700

    debugging js

--- src/public/rest.js
+++ src/public/rest.js
@@ -1,11 +1,3 @@
 function product_page(){
-    // This URL path is going to be the route defined in app.py
-    let theURL='/productInfo';
-
-    // This logger is just to keep track of the function call.
-    // You can use such log messages to debug your code if you need.
-    console.log("Making a RESTful request to the server!")
-
-    // fetch is a Javascript function that sends a request to a server
-    let response = fetch(theURL);
+    location.href = "/productInfo"
 };
